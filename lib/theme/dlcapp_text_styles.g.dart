94240e63f82d9b8f7d0f63f3da00ce206f26f6a0
Parabeac-Cloud <admin@parabeac.com>
2023-06-26 07:09:21 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/dlcapp_text_styles.g.dart
@@ -0,0 +1,30 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class DlcappTextStyles {
+
+static const TextStyle secondaryText = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle h2 = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'IBM Plex Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
